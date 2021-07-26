dd0c789f0dc30dcf5dbf39b25decc7fcc6a6ed9d
shalin <shalin.rono@student.moringaschool.com>
2021-07-26 15:21:12 +0300

    add a query selector funtion for gender

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 function naming(){
 
-    var CC=parseInt(document.getElementById("main").value);
-    var YY=parseInt(document.getElementById("name").value);
-    var MM=parseInt(document.getElementById("Form").value);
-    var DD=parseInt(document.getElementById("akan").value);
+    var CC=parseInt(document.getElementById("CC").value);
+    var YY=parseInt(document.getElementById("YY").value);
+    var MM=parseInt(document.getElementById("MM").value);
+    var DD=parseInt(document.getElementById("DD").value);
   
     var day=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Sartuday"];
     var namesFemale=["Akosua", "Adwoa","Abenaa","Akua","Yaa"," Afua","Ama"];
@@ -11,4 +11,8 @@ function naming(){
   
     if(CC.value =="" || YY.value =="" || MM.value =="" || DD.value ==""){
         alert("No blank values allowed");
+
     }
+
+    var gender = document.querySelector('input[name="gender"]:checked').value;
+    var  dayOfTheWeek = parseInt( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD )%7;
