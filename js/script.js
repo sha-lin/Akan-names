5980b28d0bce43ea312f26de0a12d486c85ac8d7
shalin <shalin.rono@student.moringaschool.com>
2021-07-26 15:24:12 +0300

    add a validate function in the form

--- js/script.js
+++ js/script.js
@@ -66,4 +66,27 @@ function naming(){
        }
      }
    }
+
+   function validate(){
+
+    var CC=document.getElementById("CC");
+    var YY=document.getElementById("YY");
+    var MM=document.getElementById("MM");
+    var DD=document.getElementById("DD");
+    // alert(DD.value)
+  
+  if((CC.value == "") || (YY.value == "") || (MM.value == "") || (DD.value == "")){
+    alert("Enter value")
+  }
+  if((MM.value <= 0) || (MM.value > 12) ){
+    alert("Enter correct month date")
+  }
+  if((DD.value <=0 ) ||(DD.value < 31) ){
+    alert("Enter correct day ")
+  }
+  else{
+    naming()
+  }
+  }
+  
    
